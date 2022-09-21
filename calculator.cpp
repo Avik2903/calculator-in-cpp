76cf74d3560770b790ed9b9e22d54efc60858ad5
Avik Sarkar <76903360+Avik2903@users.noreply.github.com>
2022-09-21 09:57:15 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -4,11 +4,9 @@
 #include<stdlib.h>  
 using namespace std;
 int main()  
-{  
-//clrscr();  
+{   
 int opr;  
-int num1, num2, x;  
-// display different operation of the calculator  
+int num1, num2, x;   
 do  
 {  
 cout << "Select an operation to perform a simple calculation in C++ Calculator"  
@@ -21,37 +19,32 @@ cout << "Select an operation to perform a simple calculation in C++ Calculator"
      "\n \n Make a choice: ";  
      cin >> opr;  
    switch (opr)  
-     {  
-     // for addition operation in calculator  
+     {   
      case 1:  
      cout << "You have selected the Addition Operation.";  
      cout << "\n Please enter the two number: \n";  
      cin >> num1 >> num2;  
      x = num1 + num2;  
      cout << "Sum of two number = " << x;  
-     break;  
-     // for subtraction operation in calculator  
+     break;    
      case 2:  
      cout << "You have selected the Subtraction Operation.";  
      cout << "\n Please enter the two number: \n";  
      cin >> num1 >> num2;  
      x = num1 - num2;  
      cout << "Subtraction of two number = " << x;  
-     break;  
-     // for multiplication operation in calculator  
+     break;   
      case 3:  
      cout << "You have selected the Multiplication Operation.";  
      cout << "\n Please enter the two number: \n";  
      cin >> num1 >> num2;  
      x = num1 * num2;  
      cout << "Product of two number = " << x;  
-     break;  
-     // for division operation in calculator  
+     break;   
      case 4:  
      cout << "You have selected the Division Operation.";  
      cout << "\n Please enter the two number; \n";  
-     cin >> num1 >> num2;  
-     // while loop checks for divisor whether it is zero  
+     cin >> num1 >> num2;    
      while ( num2 == 0)  
      {  
      cout << "\n Divisor cannot be zero"  
@@ -60,8 +53,7 @@ cout << "Select an operation to perform a simple calculation in C++ Calculator"
          }  
      x = num1 / num2;  
      cout << "\n Quotient = " << x;  
-     break;  
-     // to square a number in calculator  
+     break;    
      case 5:  
      cout << "You have selected the Square Operation.";  
       cout << "\n Please enter any number: \n";  
@@ -69,7 +61,7 @@ cout << "Select an operation to perform a simple calculation in C++ Calculator"
      x = num1 * num1;  
      cout << "Square is = " << x;  
      break;  
-     case 6: exit(0);  // terminate the program  
+     case 6: exit(0); 
      break;  
      default: cout << "\n Something went wrong..!!";  
      break;  
